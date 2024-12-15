934ec608091395bb07383228ab673323a68a39eb
sabyasachi94sen <sabyasachi9600@gmail.com>
2024-12-15 19:32:50 +0530

    updated hero section

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -9,7 +9,7 @@ export default function AboutMe() {
           <p className="section--title">About</p>
           <h1 className="skills-section--heading">About Me</h1>
           <div className="hero--section-description">
-            <p>I am a Web Designer & Front-end developer with some good Handson Project experience.I'm interested in all kinds of visual communication,but my major focus is on designing web,mobile & tablets interface.I also have skills in other fields like branding,icon design or web devlopment</p>
+            <p>I am a Front-end developer with some good Handson Project experience.I'm interested in all kinds of visual communication,but my major focus is on designing web,mobile & tablets interface.I also have skills in other fields like branding,icon design or web devlopment</p>
             <br />
             <p>
             I enjoy turning complex problems into simple,beautiful and intuitive design.When I'm not pushing pixels,you'ill find the cooking,gardening or working out in the park
